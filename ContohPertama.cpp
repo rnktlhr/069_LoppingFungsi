e82cbf290092cc96bfcc19ea31c5c0b944afaac2
rnktlhr <rinakitluhur10@gmail.com>
2024-03-18 14:29:06 +0700

    lopping menggunakan for

---
+++ ContohPertama.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int arr[5];
+
+    for(i = 0; i < 5 ; i++){
+        cout << "RINAKIT" << endl;
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Masukan Nama = ";
+        cin >> arr[i];
+    }
+
+    for(i = 0; i<5; i++){
+        cout << "Nama ke -" << i << arr[i] << endl;
+    }
+}
